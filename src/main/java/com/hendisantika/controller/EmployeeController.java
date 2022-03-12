02e2224168f0d7f65673e7dc19b78fe5154864c2
Hendi Santika <hendisantika@gmail.com>
2022-03-13 06:52:46 +0700

    #15 - Add getEmployeeById in EmployeeController

--- src/main/java/com/hendisantika/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -3,10 +3,14 @@ package com.hendisantika.controller;
 import com.hendisantika.entity.Employee;
 import com.hendisantika.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +30,10 @@ public class EmployeeController {
     @PostMapping()
     public Employee addEmployee(@RequestBody Employee emp) {
         return employeeService.saveAction(emp);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable int id) {
+        return employeeService.getEmployeeById(id);
     }
 }
