aeb2403c1d2f66ff9ad50486e312167b64cf6030
Hendi Santika <hendisantika@gmail.com>
2022-03-13 06:40:50 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-ehcache-demo
---
+++ src/main/java/com/hendisantika/SpringbootEhcacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEhcacheDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEhcacheDemoApplication.class, args);
+    }
+
+}
